078a9307589fe2197c94e36c2998370b7e1fc43d
Peer Mohamed Thabib <thabibm@gmail.com>
2018-12-26 17:43:35 +0530

    Project creation New iOS project created with Unit and UI testing targets App folder structure organized OS level debug log disabled

---
+++ DataAnalysis/DataAnalysis/ViewController/DataDisplayController.swift
@@ -0,0 +1,20 @@
+//
+//  DataDisplayController.swift
+//  DataAnalysis
+//
+//  Created by Peer Mohamed Thabib on 12/26/18.
+//  Copyright © 2018 Peer Mohamed Thabib. All rights reserved.
+//
+
+import UIKit
+
+class DataDisplayController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
